1e0976e835919e301640c92bf2a17c356f0d306a
letiiita <lrios4400@stu.d214.org>
2023-10-24 10:26:31 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Leticia Rios on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
